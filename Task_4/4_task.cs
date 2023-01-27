9523b6bc9bc1a99fe7018a9c70bb7eefebbe940a
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-28 02:20:21 +0300

    solved tasks

---
+++ Task_4/4_task.cs
@@ -0,0 +1,64 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+System.Console.WriteLine("Введите три числа:");
+
+int numberA = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int numberB = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int numberС = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int max = 0;
+
+if (numberA > numberB)
+{
+    max = numberA;
+    if (max > numberС)
+    {
+        System.Console.WriteLine($"max = {max}");
+    }
+    else if (max < numberС)
+    {
+        max = numberС;
+        System.Console.WriteLine($"max = {max}");
+    }
+
+    else
+    {
+        System.Console.WriteLine("Одного Максимального числа нет");
+    }
+
+}
+else if (numberB > numberA)
+{
+    max = numberB;
+    if (max > numberС)
+    {
+        System.Console.WriteLine($"max = {max}");
+    }
+    else if (max < numberС)
+    {
+        max = numberС;
+        System.Console.WriteLine($"max = {max}");
+    }
+
+    else
+    {
+        System.Console.WriteLine("Одного Максимального числа нет");
+    }
+}
+else
+{
+        
+    if (numberA > numberС)
+    {
+        System.Console.WriteLine("Одного Максимального числа нет");
+    }
+    else if (numberA < numberС)
+    {
+        max = numberС;
+        System.Console.WriteLine($"max = {max}");
+    }
+
+    else
+    {
+        System.Console.WriteLine("Одного Максимального числа нет");
+    }
+}
---
+++ Task_8/8task.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.WriteLine("Введите число:");
+
+int N = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+if (N > 1)
+{
+    for (int i = 2; i <= N; i += 2)
+    {
+        System.Console.Write(i + " ");
+    }
+
+}
+else
+{
+    System.Console.WriteLine("Четных положительных чисел нет");
+}
